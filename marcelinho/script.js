0363f25b9a208c5e242aa235dc22bbcf12ff5272
GABRIELLY ALENCAR MARIN <gabrielly.0165@aluno.pr.senac.br>
2025-09-26 10:19:44 -0300

    Exercícios de responsividade realidados em sala de aula

---
+++ marcelinho/script.js
@@ -0,0 +1,16 @@
+// script.js
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    const name = document.getElementById("name").value.trim();
+    const phone = document.getElementById("phone").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const message = document.getElementById("message").value.trim();
+
+    if (name && phone && email && message) {
+        document.getElementById("responseMessage").textContent = "Formulário enviado com sucesso!";
+        this.reset();
+    } else {
+        document.getElementById("responseMessage").textContent = "Por favor, preencha todos os campos.";
+    }
+});
